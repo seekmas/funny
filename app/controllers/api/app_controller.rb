3b0957133060d37e1039a6f2c2fc60c678d622be
mot <446146366@qq.com>
2015-07-21 15:35:18 +0800

    fix

--- app/controllers/api/app_controller.rb
+++ app/controllers/api/app_controller.rb
@@ -50,6 +50,34 @@ module Api
 		def get_user_task
 			#@project = Project.find_by({ :user_id => params[:user_id] })
 			@work = Work.find_by({ :user_id => params[:user_id] })
+
+			@work_categories = WorkCategory.find_by({ :work_id => @work.id })
+			if @work_categories == nil
+				@categories = TaskCatalog.all
+				@categories.each do |category|
+					new_category = WorkCategory.create({
+						:name => category.catalog_name,
+						:work_id => @work.id,
+						:is_allowed => true
+					})
+
+					@tasks = Task.where({ :task_catalog_id => category.id })
+					@tasks.each do |task|
+						WorkTask.create({
+							:task_title => task.task_title,
+							:work_id => @work.id,
+							:expired_at => nil,
+							:task_description => task.task_requirement,
+							:user_id => current_user.id,
+							:work_category_id => new_category.id,
+							:task_type_id => task.task_type_id,
+							:before_task_days => task.before_task_days,
+							:work_done => false
+						})
+					end
+				end
+			end
+
 			@tasks = []
 			if params[:task_catalog_id]
 				#@catalog = UserTaskCatalog.where(:id => params[:task_catalog_id], :project_id => @project.id ).first
@@ -95,34 +123,6 @@ module Api
 				})
 				@work.target_date = Date.today
 				@work.save
-
-				@work_categories = WorkCategory.find_by({ :work_id => @work.id })
-	      if @work_categories == nil
-	        @categories = TaskCatalog.all
-	        @categories.each do |category|
-	          new_category = WorkCategory.create({
-	            :name => category.catalog_name,
-	            :work_id => @work.id,
-	            :is_allowed => true
-	          })
-
-	          @tasks = Task.where({ :task_catalog_id => category.id })
-	          @tasks.each do |task|
-	            WorkTask.create({
-	              :task_title => task.task_title,
-	              :work_id => @work.id,
-	              :expired_at => nil,
-	              :task_description => task.task_requirement,
-	              :user_id => current_user.id,
-	              :work_category_id => new_category.id,
-	              :task_type_id => task.task_type_id,
-	              :before_task_days => task.before_task_days,
-	              :work_done => false
-	            })
-	          end
-	        end
-	      end
-
 		  	# @project = Project.find_or_create_by({ :user_id => @user.id })
 		    # if @project.project_date == nil
 		    #   @project.project_date = DateTime.now
